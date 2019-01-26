1544146c4e36c057db621c7516bc36b9cdfd87ad
Peter Tucker <ptucker@whitworth.edu>
2019-01-26 15:30:25 -0800

    Update examTest.java

--- examTest.java
+++ examTest.java
@@ -1,3 +1,21 @@
+/*
+FEEDBACK
+
+Read from the file correctly: 20/20%
+Implement class hierarchy correctly: 20/20%
+Populate appropriate data structures with shape data: 15/15%
+Show list of shapes correctly: 15/15%
+Show specific shape properties correctly: 8/15%
+Well-structured, well-commented code: 10/15%
+
+
+Comments:
+  Break out createUIComponents into smaller functions
+  Break out loadMaps into a separate class
+  Shape doesn't need to define toString since it's on Object
+  Not seeing shape details
+*/
+
 // A class to test the functionality of this GUI 
 
 public class examTest {
